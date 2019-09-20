b6fdf147dbc8e17c982aaaffbf5151a35774a9ab
Connor Holly <connorholly11@gmail.com>
2019-09-20 09:20:30 -0600

    created private route and axiosWithAuth, everything routed in app, starting login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,10 +3,26 @@ import React from "react";
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [form, setForm] = useState({username: '', password: ''})
+
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <input  
+        name='username'
+        placeholder='username'
+        onChange={}
+        value={}
+        />
+        <input  
+        name='password'
+        placeholder='password'
+        onChange={}
+        value={}
+        />
+      </form>
     </>
   );
 };
