9ab11c6f120da65d8aeaf3605e7f620bb6478c43
Connor Holly <connorholly11@gmail.com>
2019-09-20 09:33:44 -0600

    done with login page and token

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
   const [form, setForm] = useState({username: '', password: ''})
 
+  const ChangeHandler = e => {
+    setForm ({...form, [e.target.name]: e.target.value})
+ }
+
+  const SubmitForm = e => {
+    e.preventDefault()
+    axiosWithAuth()
+      .post('/login', form)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload)
+
+        props.history.push('/bubblepage')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   return (
     <>
-      <form>
-        <input  
-        name='username'
-        placeholder='username'
-        onChange={}
-        value={}
-        />
-        <input  
-        name='password'
-        placeholder='password'
-        onChange={}
-        value={}
-        />
+      <form onSubmit = {SubmitForm} >
+          <input  
+          name='username'
+          placeholder='username'
+          onChange={ChangeHandler}
+          value={form.username}
+          />
+
+          <input  
+          name='password'
+          placeholder='password'
+          onChange={ChangeHandler}
+          value={form.password}
+          />
+
+          <button type='submit'> Login </button>
       </form>
     </>
   );
